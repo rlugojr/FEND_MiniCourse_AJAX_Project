475a6c84dd9257ed4e24978092737a678dc325cc
Ray Lugo, Jr <rlugojr@gmail.com>
2016-07-18 23:38:34 -0400

    updates
    Still chugging along.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-function loadData() {
-
+function loadData(e) {
+    e.preventDefault();
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -12,10 +12,36 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var gsvkey = 'AIzaSyB4Sx-EmG0FVrUBwkOuk9Btwv2cW3jjuZg';
+    var strStreet = $('#street').val();
+    var strCity = $('#city').val();
+    var address = strStreet + ', ' + strCity;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So, you want to live at ' + address + '?');
+    strStreetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
 
-    return false;
-};
+    $body.append('<img class="bgimg" src="'+strStreetViewURL+'">');
+
+    //NY Times - get articles
+    var nytArticleURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+strCity+'&sort=newest&api-key=8d7a9993eaab46ae990a74b3f755faa6';
+
+    $.getJSON(nytArticleURL, function( data ) {
+
+        $nytHeaderElem.text('New York Times Articles About '+strCity);
+
+        articles = data.response.docs;
+
+        for (var i=0; i < articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+ '</li>');
+        }
+
+        return false;
+    }).fail(function() {
+    	$nytHeaderElem.text('New York Times Articles could not be loaded at this time.');
+  });
+}
 
 $('#form-container').submit(loadData);
